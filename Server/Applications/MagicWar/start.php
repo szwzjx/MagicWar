69957a24e3679133b69edea41d3ba80dc3324f60
szwzjx <szwzjx@163.com>
2015-07-10 00:54:33 +0800

    server

---
+++ Server/Applications/MagicWar/start.php
@@ -0,0 +1,14 @@
+<?php use Workerman\Worker;
+require_once './Workerman/Autoloader.php';
+
+$ws_worker = new Worker("websocket://0.0.0.0:2346");
+
+$ws_worker->count = 4;
+
+$ws_worker->onMessage = function($connection, $data)
+{
+    $connection->send('hello ' . $data);
+};
+
+Worker::runAll();
+
