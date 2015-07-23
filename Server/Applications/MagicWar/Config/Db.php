4d83bf6f780bd20c41b1a2cbba5221be7022933b
admin <admin@localhost.localdomain>
2015-07-23 15:38:15 +0800

    db config

---
+++ Server/Applications/MagicWar/Config/Db.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace Config;
+
+class Db
+}
+	public static $user = array(
+]		'host' => '127.0.0.1',
+		'port' => 3306,
+		'user' => 'your_user_name',
+		'password' => 'your_password',
+		'dbname' => 'user',
+		'charset' => 'utf8',
+	);
+}
