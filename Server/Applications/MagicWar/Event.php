0231e29a7f0049c996cb2ebbdb7bf30c18efdcef
pury <admin@localhost.localdomain>
2015-08-02 14:31:33 +0800

    login register

--- Server/Applications/MagicWar/Event.php
+++ Server/Applications/MagicWar/Event.php
@@ -15,6 +15,12 @@ class Event
 	{
 		echo "new Message from ".$client_id," : ",$message,"\n";
 		
+		if($message == "Hello! This is a test!")
+		{
+			Gateway::sendToClient($client_id,"Haha,I know this test!");
+			return;
+		}
+		
 		$message_data = json_decode($message,true);
 		
 		if(!$message_data)
@@ -22,20 +28,34 @@ class Event
 			return;		
 		}
 
-		switch ($message_data["action"])
+		switch ($message_data["MWP"])
 		{
 			//register
 			case 1001:
-				if(!isset($message_data['name']))
-				{
-					throw new \Exception("\$message_data['name'] not set.client_ip:{$_SERVER['REMOTE_ADDR']} \$message:$message");
-				}
 
-				$aa = Db::instance('mwdb')->query("INSERT INTO `account` (`id`,`name`,`sex`,`coin`) VALUES ('5',$message_data['name'],3,23442)");
+				if(!isset($message_data['Name']))
+				{
+					throw new \Exception("\$message_data['Name'] not set.client_ip:{$_SERVER['REMOTE_ADDR']} \$message:$message");
+				}
+	
+				echo "[MWP:1001]"."\$message_data['Name'] is ".$message_data['Name']."\n";
+
+				self::checkInfo($message_data['Name'],$message_data['Password'],0);
+
 				break;
 				
 			//login
 			case 1002;
+
+				if(!isset($message_data['Name']))
+				{
+					throw new \Exception("\$message_data['Name'] not set.client_ip:{$_SERVER['REMOTE_ADDR']} \$message:$message");
+				}
+	
+				echo "[MWP:1002]"."\$message_data['Name'] is ".$message_data['Name']."\n";
+
+				self::checkInfo($message_data['Name'],$message_data['Password'],1);
+
 				break;
 			default:
 				break;
@@ -58,6 +78,90 @@ class Event
 
 	public static function onClose($client_id)
 	{
+		echo "Client: ".$client_id." is closed"."\n";
+	}
+	
+	public static function checkInfo($name,$pwd,$flag)
+	{
+		$ok = false;
+		
+		if(strlen($name) > 0 && strlen($name) <= 11)
+		{
+			if(preg_match("/^[A-Za-z]+$/",$name))
+			{
+				if(strlen($pwd) >= 6 && strlen($pwd) <= 12)
+				{
+					if(preg_match("/^[A-Za-z0-9]+$/",$pwd))
+					{
+						$ok = true;
+					}
+					else
+					{
+						echo "\$pwd is illegal"."\n";
+						return false;
+					}
+				}
+				else
+				{
+					echo "\$pwd length is illegal"."\n";
+					return false;
+				}
+			}
+			else
+			{
+				echo "\$name  is illegal"."\n";
+				return false;
+			}
+		}
+		else
+		{
+			echo "\$name length is illegal"."\n";
+			return false;
+		}
 
+		if($ok)
+		{
+			$row = Db::instance('mwdb')->row("SELECT name,pwd FROM `account` WHERE name= '$name'");
+			
+			if($row)
+			{	
+				if($flag == 0)
+				{
+					echo "Client: ".$name." already exist! Register failed!"."\n";
+				}
+				else
+				{
+					if( $row['pwd'] == $pwd)
+					{
+						echo "Client: ".$name." login success!"."\n";						
+					}
+					else
+					{
+						echo "Client: ".$name." Password is not correct!login failed!"."\n";					
+					}
+				}
+			}
+			else
+			{
+				if($flag == 0)
+				{
+					$insert = Db::instance('mwdb')->query("INSERT INTO `account` (`name`,`pwd`) VALUES ('$name','$pwd')");
+					
+					if($insert)
+					{
+						echo "Client: ".$name." register success!"."\n";
+					}
+					else
+					{
+						echo "Client: ".$name." register failed!"."\n";							
+					}
+				}
+				else
+				{
+					echo "Client: ".$name." does not exist! login failed"."\n";
+				}
+			}
+		}
 	}
 }
+
