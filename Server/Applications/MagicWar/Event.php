3933e3d9eeea2660b486f8bac7a4b933f03b5cb9
pury <admin@localhost.localdomain>
2015-08-13 00:33:48 +0800

    create role

--- Server/Applications/MagicWar/Event.php
+++ Server/Applications/MagicWar/Event.php
@@ -42,28 +42,37 @@ class Event
 	
 				echo "[MWP:1001]"."\$message_data['Name'] is ".$message_data['Name']."\n";
 
-				self::checkInfo($message_data['Name'],$message_data['Password'],0);
+				self::do_login_or_register($client_id,$message_data['Name'],$message_data['Password'],0);
 
 				break;
 				
 			//login
-			case 1002;
+			case 1002:
 
 				if(!isset($message_data['Name']))
 				{
 					throw new \Exception("\$message_data['Name'] not set.client_ip:{$_SERVER['REMOTE_ADDR']} \$message:$message");
 				}
 
-				self::checkInfo($message_data['Name'],$message_data['Password'],1);
+				self::do_login_or_register($client_id,$message_data['Name'],$message_data['Password'],1);
+
+				break;
+			//create role
+			case 1003:
+					
+				if(isset($message_data['RoleName']) && isset($message_data['Gender']))
+				{
+					self::create_role($client_id,$message_data['RoleName'],$message_data['Gender']);
+				}
 
 				break;
 
-			//player information
-			case 1003:
+			//role information
+			case 1004:
 				
 				if(isset($_SESSION['uid']))
 				{
-					self::getRole($client_id,$_SESSION['uid']);
+					self::get_role($client_id,$_SESSION['uid']);
 				}
 				else
 				{
@@ -84,10 +93,11 @@ class Event
 	}
 	
 	//--------------------------------------------------------------------------------
-	public static function checkInfo($name,$pwd,$flag)
+	public static function do_login_or_register($client_id,$name,$pwd,$flag)
 	{
 		$ok = false;
-		
+		$data = Array();
+
 		if(strlen($name) > 0 && strlen($name) <= 11)
 		{
 			if(preg_match("/^[A-Za-z]+$/",$name))
@@ -124,24 +134,27 @@ class Event
 
 		if($ok)
 		{
-			$row = Db::instance('mwdb')->row("SELECT uid,name,pwd FROM `account` WHERE name= '$name'");
+			$row = Db::instance('mwdb')->row("SELECT uid,name,password FROM `account` WHERE name= '$name'");
 			
 			if($row)
 			{	
 				if($flag == 0)
 				{
 					echo "Client: ".$name." already exist! Register failed!"."\n";
+					$data = Array('mwp' => 1002,'data' => Array('errcode' => 1,'errormsg' => 'Register failed!'));
 				}
 				else
 				{
-					if( $row['pwd'] == $pwd)
+					if( $row['password'] == $pwd)
 					{
 						echo "Client: ".$name." login success!"."\n";		
-						$_SESSION['uid'] = $row['uid']; 				
+						$_SESSION['uid'] = $row['uid']; 	
+						$data = Array('mwp' => 1001,'data' => Array('errcode' => 0,'errormsg' => ''));
 					}
 					else
 					{
-						echo "Client: ".$name." Password is not correct!login failed!"."\n";					
+						echo "Client: ".$name." Password is not correct!login failed!"."\n";	
+						$data = Array('mwp' => 1001,'data' => Array('errcode' => 1,'errormsg' => 'Password is not correct!Login failed!'));
 					}
 				}
 			}
@@ -149,41 +162,86 @@ class Event
 			{
 				if($flag == 0)
 				{
-					$insert = Db::instance('mwdb')->query("INSERT INTO `account` (`name`,`pwd`) VALUES ('$name','$pwd')");
+					$insert = Db::instance('mwdb')->query("INSERT INTO `account` (`name`,`password`) VALUES ('$name','$pwd')");
 					
 					if($insert)
 					{
 						echo "Client: ".$name." register success!"."\n";
+						$_SESSION['uid'] = $row['uid']; 
+						$data = Array('mwp' => 1002,'data' => Array('errcode' => 0,'errormsg' => ''));
 					}
 					else
 					{
-						echo "Client: ".$name." register failed!"."\n";							
+						echo "Client: ".$name." register failed!"."\n";	
+						$data = Array('mwp' => 1002,'data' => Array('errcode' => 1,'errormsg' => 'Register failed!'));						
 					}
 				}
 				else
 				{
 					echo "Client: ".$name." does not exist! login failed"."\n";
+					$data = Array('mwp' => 1001,'data' => Array('errcode' => 1,'errormsg' => 'Login failed!'));
 				}
 			}
+		}
+		
+		self::send_to_client($client_id,$data);
+	}
+
+	//--------------------------------------------------------------------------------
+	public static function create_role($client_id,$rolename,$gender)
+	{
+		$data = Array('mwp' => 1003,'data' => Array('errcode' => 1,'errormsg' => 'create role failed'));
+
+		if($_SESSION['uid'] > 0)
+		{
+			$uid = $_SESSION['uid'];
+			$row = Db::instance('mwdb')->row("SELECT accountid FROM `player` WHERE playername= '$rolename'");
+			$row2 = Db::instance('mwdb')->row("SELECT playername FROM `player` WHERE accountid= '$uid'");
+
+			if(!$row && !$row2)
+			{
+				
+				$que = Db::instance('mwdb')->row("SELECT uid,name FROM `account` WHERE uid= $uid");
+				$name = $que['name'];
+				$insert = Db::instance('mwdb')->query("INSERT INTO `player` (`playername`,`accountid`,`accountname`,`gender`) VALUES ('$rolename',$uid,'$name',$gender)");
+
+				if($insert == 0)
+				{
+					$data = Array('mwp' => 1003,'data' => Array('errcode' => 0,'errormsg' => ''));
+				}
+			}
+		}
+
+		self::send_to_client($client_id,$data);
+		
+		if($data['data']['errcode'] == 0)
+		{
+			self::get_role($client_id,$uid);
 		}
 	}
 
 	//--------------------------------------------------------------------------------
-	public static function getRole($client_id,$uid)
+	public static function get_role($client_id,$uid)
 	{
-		$row = Db::instance('mwdb')->row("SELECT uid,name,sex,head,coin,lv FROM `account` WHERE uid= '$uid'");
-		
+		$row = Db::instance('mwdb')->row("SELECT playername,lv,exp,coin,gender,headicon FROM `player` WHERE accountid= $uid");
+		$data = Array('mwp'=>1004,'data'=>Array('errcode'=>1,'errormsg'=>'the role is not found'));
+
 		if($row)
 		{
-			$data = Array('mwp'=>1003,'data'=>Array('errcode'=>0,'errormsg'=>'','name'=>$row['name'],'sex'=>$row['sex'],'head'=>$row['head'],'coin'=>$row['coin'],'lv'=>$row['lv']));
-		}		
-		else
-		{
-			$data = Array('mwp'=>1003,'data'=>Array('errcode'=>1,'errormsg'=>'the role is not found'));
+			$data = Array('mwp' => 1004,'data'=>Array('errcode'=> 0,'errormsg' => '','Playername' => $row['playername'],'Lv' => $row['lv'],'Exp' => $row['exp'],'Coin' => $row['coin'],'Gender' => $row['gender'],'Headicon' => $row['headicon']));
 		}
 
-		$msg = json_encode($data);
-		Gateway::sendToClient($client_id,$msg);
+		self::send_to_client($client_id,$data);
+	}
+
+	//--------------------------------------------------------------------------------
+	public static function send_to_client($client_id,$data)
+	{
+		if($client_id > 0 && $data)
+		{
+			$msg = json_encode($data);
+			Gateway::sendToClient($client_id,$msg);
+		}
 	}
 }
 
